369178ed67cfa028df585f4512f13501021c87cf
Mauri de Souza Nunes <mauri870@gmail.com>
2016-09-02 19:16:35 -0300

    Update client methods

--- client/main.go
+++ client/main.go
@@ -1,39 +1,29 @@
 package client
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 const (
 	SERVER_URL = "http://localhost:8080"
 )
 
-var (
-	ErrorBadJson = errors.New("Bad response content from server, expecting valid JSON")
-)
+// Call the server
+func CallServer(method string, endpoint string, data url.Values) (*http.Response, error) {
+	req, err := http.NewRequest(method, SERVER_URL+endpoint, strings.NewReader(data.Encode()))
+	if err != nil {
+		return new(http.Response), err
+	}
 
-// Make a request to server
-func CallServer(endpoint string) (*http.Response, error) {
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET", fmt.Sprintf("%s/%s", SERVER_URL, endpoint), nil)
-	req.Header.Set("User-Agent", "Ransomware/1.0")
 	res, err := client.Do(req)
 	if err != nil {
 		return new(http.Response), err
 	}
 
 	return res, nil
-}
-
-// Parse a Json response from server
-func ParseServerJson(body []byte) (map[string]string, error) {
-	var content map[string]string
-	if err := json.Unmarshal(body, &content); err != nil {
-		return content, ErrorBadJson
-	}
-
-	return content, nil
 }

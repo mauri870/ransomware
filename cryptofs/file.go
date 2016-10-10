36cd4076ecc0eff87c6510d523d1b300e5df80e6
Mauri de Souza Nunes <mauri870@gmail.com>
2016-10-09 22:32:58 -0300

    [cryptofs] Add cryptofs package

---
+++ cryptofs/file.go
@@ -0,0 +1,91 @@
+// Package cryptofs provides a basic abstraction for file encryption/decryption
+package cryptofs
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+	"os"
+)
+
+// Type File wrap an os.FileInfo
+type File struct {
+	os.FileInfo
+	Extension string // The file extension without dot
+	Path      string // The absolute path of the file
+}
+
+// Encrypt the file content with AES-CTR with the given key
+// sending then to dst
+func (file *File) Encrypt(enckey string, dst *os.File) error {
+	// Open the file read only
+	inFile, err := os.Open(file.Path)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	// Create a 128 bits cipher.Block for AES-256
+	block, err := aes.NewCipher([]byte(enckey))
+	if err != nil {
+		return err
+	}
+
+	// The IV needs to be unique, but not secure
+	iv := make([]byte, aes.BlockSize)
+	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
+		return err
+	}
+
+	// Get a stream for encrypt/decrypt in counter mode (best performance I guess)
+	stream := cipher.NewCTR(block, iv)
+
+	// Write the Initialization Vector (iv) as the first block
+	// of the destination file
+	dst.Write(iv)
+
+	// Open a stream to encrypt and write to destination file
+	writer := &cipher.StreamWriter{S: stream, W: dst}
+
+	// Copy the input file to the destination file, encrypting as we go.
+	if _, err = io.Copy(writer, inFile); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Decrypt the file content with AES-CTR with the given key
+// sending then to dst
+func (file *File) Decrypt(key string, dst *os.File) error {
+	// Open the encrypted file
+	inFile, err := os.Open(file.Path)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	// Create a 128 bits cipher.Block for AES-256
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return err
+	}
+
+	// Retrieve the iv from the encrypted file
+	iv := make([]byte, aes.BlockSize)
+	inFile.Read(iv)
+
+	// Get a stream for encrypt/decrypt in counter mode (best performance I guess)
+	stream := cipher.NewCTR(block, iv)
+
+	// Open a stream to decrypt and write to output file
+	reader := &cipher.StreamReader{S: stream, R: inFile}
+
+	// Copy the input file to the destination file, decrypting as we go.
+	if _, err = io.Copy(dst, reader); err != nil {
+		return err
+	}
+
+	return nil
+}
